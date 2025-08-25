7c57513e4f25dfe5e4e5578d4c328925ad061052
Himabindu Banda <himabindhubanda93@gmail.com>
2025-08-24 22:34:11 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementByld("registrationForm").addEventListener("submit", function (e) { e.preventDefault();
+
+// Simulating successful registration
+const successMessage = document.getElementByld("successMessage"); successMessage.classList.remove("hidden'');
+
+// Resetting the form this.reset();
+});
